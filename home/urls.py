98dacd358262bf868492e50b46536c865f2a6d5f
SYN606 <postbox.501@yandex.com>
2025-12-02 11:40:37 +0530

    seprated templates

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage),
-    path('login', views.login)
+    path('', views.homepage, name='homepage'),
+    path('login', views.login, name='login')
 ]
--- myproject/settings.py
+++ myproject/settings.py
@@ -40,7 +40,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'kuch_bhi')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
