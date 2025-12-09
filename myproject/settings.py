e0a9d8c40746b5205ae433a0cab6616d8e2ae4b4
SYN606 <postbox.501@yandex.com>
2025-12-09 11:34:42 +0530

    started a new project

--- myproject/settings.py
+++ myproject/settings.py
@@ -22,7 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home'
+    'home',
+    'devdetails',
 ]
 
 MIDDLEWARE = [
@@ -87,7 +88,6 @@ TIME_ZONE = 'Asia/Kolkata'
 USE_I18N = True
 
 USE_TZ = True
-
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
